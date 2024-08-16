da46d2a66f227b0ca0a8e38435c3aed1a74f8a05
Kaustubh-zelar <kaustubh.shakelli@Zelarsoft.com>
2024-08-16 11:13:59 +0530

    worked on some oops concepts

--- Oop.py
+++ Oop.py
@@ -1,1 +1,30 @@
-let fruits = ["Mango", "Strawberries", "Lime", "Oranges", "Pomegranate"];
+class Employee:
+    num_of_emps=0
+    raise_amt= 1.04
+
+
+    def __init__(self, first,last,pay):
+        self.first= first
+        self.last= last
+        self.pay= pay
+        self.email=first + '.'+ last + "@comapany.com"
+
+        Employee.num_of_emps+=1
+    
+    def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+    
+    def apply_raise(self):
+        self.pay= int(self.pay*self.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls,amount):
+        pass
+
+
+
+emp_1 = Employee('Kaustubh','shakelli', 50000)
+emp_2 = Employee('Test','Run',90000)
+
+
+
--- script.js
+++
@@ -1,7 +0,0 @@
-const nums = [1, 2, 3, 5, 6];
-nums.fill(0); 
-console.log(nums);
-
-const strings = ["code", "damn", "web2", "web3"];
-strings.fill("abc", 2+1-2, 4); 
-console.log(strings);
