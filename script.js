a4777b704f956258d3ed4c0a0f8e5ce7a567c3e0
Kaustubh-zelar <kaustubh.shakelli@Zelarsoft.com>
2024-08-20 18:22:45 +0530

    learning string manipulation

---
+++ script.js
@@ -0,0 +1,24 @@
+// let str = "JavaScript";
+// let result = str.substring(0, 4);
+// console.log(result);
+
+// let str = "Hello World";
+// let result = str.replace("World", "JavaScript");
+// console.log(result);
+// let str = "   Hello World   ";
+// let result = str.trim();
+// console.log(result);
+
+
+// let str = "apple,banana,cherry";
+// let result = str.split(",");
+// console.log(result);
+
+// let arr = ["apple", "banana", "cherry"];
+// let result = arr.join(", ");
+// console.log(result);
+
+
+let str = "Hello World";
+let result = str.includes("World");
+console.log(result);
